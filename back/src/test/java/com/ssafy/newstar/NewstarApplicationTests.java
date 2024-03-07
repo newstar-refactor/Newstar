4dddb93126723b7f78b80b2c0271b29bf1ff03ad
최준영 <wjdghkswnsdu@naver.com>
2024-03-07 10:28:41 +0900

    build: back spring init

---
+++ back/src/test/java/com/ssafy/newstar/NewstarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.newstar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewstarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
