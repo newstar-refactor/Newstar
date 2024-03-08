7e97beb791070a5a6cadc2ad6beb5c0e36cdccb1
최준영 <wjdghkswnsdu@naver.com>
2024-03-08 17:44:28 +0900

    feat: fastApi 서버 크롤링 api 호출 구현

--- back/src/main/java/com/ssafy/newstar/global/scheduler/SchedulerConfiguration.java
+++ back/src/main/java/com/ssafy/newstar/global/scheduler/SchedulerConfiguration.java
@@ -4,13 +4,25 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class SchedulerConfiguration {
-  @Scheduled(cron = "0 5 * * * *") // 매일 매시 05분에 실행
+
+  private final WebClient webClient = WebClient.create("http://localhost:8000");
+  @Scheduled(cron = "0 30 * * * *") // 매일 매시 05분에 실행
   public void doCrawling() {
     log.info("크롤링 ing");
+
+    Mono<String> response = webClient.get()
+        .uri("/crawling")
+        .retrieve()
+        .bodyToMono(String.class);
+
+    response.subscribe(result -> log.info(result),
+        error -> log.error("Error: " + error.getMessage()));
   }
 }
