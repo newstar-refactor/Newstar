d4067169993c5a69d259b96383e9b53b5b8a56a5
박은미 <amy0231@naver.com>
2024-03-11 13:13:18 +0900

    feat: button atom 설계

--- front/newstar/src/common/Like.jsx
+++ front/newstar/src/common/Like.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { FaBookmark, FaRegBookmark } from "react-icons/fa";
 
-const LikeButton = ({ handleButtonClick }) => {
+const LikeButton = ({ handleLikeButtonClick }) => {
 
   // isLiked가 true이면, 채워진 북마크 / false면, 빈 북마크 활성화
   const likeBtn = isLiked ? (
-    <FaBookmark onClick={handleButtonClick} />
+    <FaBookmark onClick={handleLikeButtonClick} />
   ) : (
-    <FaRegBookmark onClick={handleButtonClick} />
+    <FaRegBookmark onClick={handleLikeButtonClick} />
   );
 
   return (
@@ -15,6 +15,6 @@ const LikeButton = ({ handleButtonClick }) => {
       {likeBtn}
     </div>
   )
-}
+};
 
 export default LikeButton;
