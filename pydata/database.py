570e33a968076c0c88be4bc662e5222f3a41f5ae
최준영 <wjdghkswnsdu@naver.com>
2024-03-11 17:19:56 +0900

    feat: DB 커넥션 구현

--- pydata/database.py
+++ pydata/database.py
@@ -4,23 +4,28 @@ from sqlalchemy import create_engine
 import json
 import os
 
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-SECRET_FILE = os.path.join(BASE_DIR, 'secret.json')
-secrets = json.loads(open(SECRET_FILE).read())
+SECRET_FILE = os.path.join(BASE_DIR, 'secrets.json')
+
+with open(SECRET_FILE) as f:
+  secrets = json.load(f)
+
 DB = secrets['DB']
 
 DB_URL = f"mysql+pymysql://{DB['user']}:{DB['password']}@{DB['host']}/{DB['database']}?charset=utf8"
 
-engine = create_engine(
-    DB_URL, encodings = 'utf-8'
-)
+engine = create_engine(DB_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
 def get_db():
   db = SessionLocal()
   try:
     yield db
   finally:
     db.close()
+
+
