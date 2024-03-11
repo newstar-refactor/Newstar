997284a95b54b09077faa36711ac22cd38fcd8f8
최준영 <wjdghkswnsdu@naver.com>
2024-03-11 17:21:00 +0900

    test: api 호출 DB 커넥션 test

--- pydata/main.py
+++ pydata/main.py
@@ -1,6 +1,12 @@
-from fastapi import FastAPI
+from cgitb import text
 
+from fastapi import FastAPI
 from category_crawling import do_crawling
+from database import get_db, engine
+from sqlalchemy.orm import sessionmaker
+
+Session = sessionmaker(bind=engine)
+session = Session()
 
 app = FastAPI()
 
@@ -18,3 +24,20 @@ async def say_hello(name: str):
 async def start_crawling():
   do_crawling()
   return {"message": "complete crawling"}
+
+# Path: category_cra
+
+@app.get("/member" )
+def read_items():
+  connection = next(get_db())
+  try:
+    print("1")
+    # SQL 쿼리
+    # result = session.query(Member).all()
+    # result = connection.execute(text("SELECT * FROM member"))
+    # member = result.first()
+    # print(member)
+  finally:
+    # 데이터베이스 연결 종료
+    connection.close()
+
