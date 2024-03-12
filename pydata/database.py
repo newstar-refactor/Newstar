b8455736cc627bdd52d4c631c9f8f7bcdc663b10
최준영 <wjdghkswnsdu@naver.com>
2024-03-12 12:59:19 +0900

    feat: mount 메소드 추가

--- pydata/database.py
+++ pydata/database.py
@@ -1,9 +1,8 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 import json
 import os
-
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 SECRET_FILE = os.path.join(BASE_DIR, 'secrets.json')
@@ -12,13 +11,11 @@ with open(SECRET_FILE) as f:
   secrets = json.load(f)
 
 DB = secrets['DB']
-
 DB_URL = f"mysql+pymysql://{DB['user']}:{DB['password']}@{DB['host']}/{DB['database']}?charset=utf8"
 
 engine = create_engine(DB_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 def get_db():
@@ -27,5 +24,3 @@ def get_db():
     yield db
   finally:
     db.close()
-
-
