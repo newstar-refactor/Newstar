e5a938b567f7e2ae2bc655b7602eec8c28ab87ea
한성현 <tjdgus4560@naver.com>
2024-03-13 09:34:38 +0900

    fix : venv 삭제

---
+++ pydata/services/learning/news_doc2vec.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from konlpy.tag import Okt  # Okt로 변경
+from mecab import MeCab
+from gensim.models.doc2vec import TaggedDocument
+from tqdm import tqdm
+
+import re
+from gensim.models import doc2vec
+
+def makeModel():
+    news_df = pd.read_csv('dataset/category_articles.csv')
+    news_df = news_df.dropna()
+
+    mecab = MeCab()
+
+    tagged_corpus_list = []
+
+    for index, row in tqdm(news_df.iterrows(), total=len(news_df)):
+        text = row['content']
+        # 본문 기호 제거
+        text = re.sub('[-=+,#/\?:^.@*\"※~ㆍ!』‘|\(\)\[\]`\'…》\”\“\’·]', ' ', text)
+        tag = row['title']
+        tagged_corpus_list.append(TaggedDocument(tags=[tag], words=mecab.nouns(text)))
+
+    model = doc2vec.Doc2Vec(vector_size=300, alpha=0.025, min_alpha=0.025, workers=16, window=5)
+
+    # Vocabulary 빌드
+    model.build_vocab(tagged_corpus_list)
+
+    # Doc2Vec 학습
+    model.train(tagged_corpus_list, total_examples=model.corpus_count, epochs=10)
+
+    # 모델 저장
+    model.save('dart.doc2vec')
+
+    return {"message" : "model is ready"}
