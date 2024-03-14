f349a77447373373c8bb041251fb64e04dc527a6
박은미 <amy0231@naver.com>
2024-03-14 13:51:12 +0900

    refactor: keyword를 보낼때만 뉴스리스트 요청

--- front/newstar/src/components/SearchBar.jsx
+++ front/newstar/src/components/SearchBar.jsx
@@ -1,13 +1,50 @@
 // 검색바
+import React, { useState } from 'react';
+import { IoSearch } from "react-icons/io5";
+import styled from 'styled-components';
 
-export default function SearchBar({setKeyword}) {
+// 검색 바 컨테이너 스타일
+const SearchBarContainer = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+`;
+
+// 입력 필드 스타일
+const Input = styled.input`
+  width: 100%;
+  padding-right: 30px; /* 아이콘 공간 확보 */
+`;
+
+// 검색 아이콘 스타일
+const SearchIcon = styled(IoSearch)`
+  position: absolute;
+  right: 10px;
+  cursor: pointer;
+`;
+
+export default function SearchBar({ setKeyword, onSearch }) {
+
+  const [input, setInput] = useState('');
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleSearchClick = () => {
+    setKeyword(input);
+    onSearch();
+  }
+
   return (
-    <div>
-      <input 
+    <SearchBarContainer>
+      <Input
         type="text"
+        value={input}
         placeholder="키워드를 입력하세요."
-        onChange={(e) => setKeyword(e.target.value)}
-         />
-    </div>
-  )
+        onChange={handleInputChange}
+      />
+      <SearchIcon onClick={handleSearchClick} />
+    </SearchBarContainer>
+  );
 }
--- front/newstar/src/pages/Search.jsx
+++ front/newstar/src/pages/Search.jsx
@@ -4,7 +4,6 @@
 import React, { useState, useEffect } from 'react';
 // import api from '../api/api'
 // import axios from 'axios';
-
 import SearchBar from '../components/SearchBar'
 import SearchNewsList from '../components/main/SearchNewsList'
 
@@ -16,14 +15,13 @@ export default function Search() {
   // 필터링 된 뉴스 목록 상태관리
   const [filteredNews, setFilterdNews] = useState([]);
 
-  // keyword가 변경될 때 마다 실행
-  // 영어 keyword가 입력될 경우를 대비하여 모두 소문자로 변환 후 비교
-  useEffect(() => {
-
+  // 뉴스 데이터 가져오는 함수
+  const fetchNews = () => {
+    // 공백 제거 및 소문자 변환
     const searchKeyword = keyword.trim().toLowerCase();
 
-    if (searchKeyword.trim() !== '') {
-      // ** url 수정해야함 **
+    // 검색어가 비어있지 않은 경우에만 API 요청 수행
+    if (searchKeyword !== '') {
       axios.get(`${api.news}?keyword=${searchKeyword}`)
         .then(response => {
           setFilterdNews(response.data);
@@ -31,17 +29,15 @@ export default function Search() {
         .catch(error => {
           console.log("Error fetching Searchdata", error);
         });
-      // 키워드가 비어있으면 뉴스 목록 비우기
     } else {
       setFilterdNews([]);
     }
-  }, [keyword])
-
+  };
 
   return (
     <div>
       <div>검색페이지</div>
-      <SearchBar setKeyword={setKeyword}/>
+      <SearchBar setKeyword={setKeyword} onSearch={fetchNews}/>
       <SearchNewsList NewsData={filteredNews} />
     </div>
   )
