a7effe8bdf1fb39aa6db482f26d80338fb3fe42c
최준영 <wjdghkswnsdu@naver.com>
2024-03-15 16:30:01 +0900

    feat: 로그인 filter 구현

--- back/src/main/java/com/ssafy/newstar/global/config/FilterConfig.java
+++ back/src/main/java/com/ssafy/newstar/global/config/FilterConfig.java
@@ -1,5 +1,6 @@
 package com.ssafy.newstar.global.config;
 
+import com.ssafy.newstar.domain.member.repository.MemberRepository;
 import com.ssafy.newstar.global.filter.MemberAuthenticationFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -9,9 +10,9 @@ import org.springframework.context.annotation.Configuration;
 public class FilterConfig {
 
     @Bean
-    public FilterRegistrationBean<MemberAuthenticationFilter> memberAuthenticationFilter() {
+    public FilterRegistrationBean<MemberAuthenticationFilter> memberAuthenticationFilter(MemberRepository memberRepository) {
         FilterRegistrationBean<MemberAuthenticationFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(new MemberAuthenticationFilter());
+        registrationBean.setFilter(new MemberAuthenticationFilter(memberRepository));
         registrationBean.addUrlPatterns("/*"); // 모든 URL에 필터를 적용
         return registrationBean;
     }
