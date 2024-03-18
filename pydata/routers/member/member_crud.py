4fa9955a140173a25e725ee91f59d1b68a17d953
한성현 <tjdgus4560@naver.com>
2024-03-18 14:55:19 +0900

    feat : 좋아요 기준 뉴스 추천구현

---
+++ pydata/routers/member/member_crud.py
@@ -0,0 +1,10 @@
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
+from models import Member
+
+async def get_user_by_pw(db: Session, pw: str):
+    result = db.query(Member).with_entities(Member.member_id).filter(Member.pw == pw).first()
+    if result is None:
+        raise HTTPException(status_code=404, detail="Member not found")
+    member_id = result.member_id
+    return member_id
