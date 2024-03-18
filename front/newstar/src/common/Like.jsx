3606ae77facd91b14e8031c45c460a98629f0756
박은미 <amy0231@naver.com>
2024-03-18 13:07:35 +0900

    feat: newscard 더블클릭하면 좋아요 기능

--- front/newstar/src/common/Like.jsx
+++ front/newstar/src/common/Like.jsx
@@ -3,26 +3,28 @@ import Lottie from "lottie-react"
 
 import likeLottie from "../assets/lottie/like.json"
 
-import { FaBookmark, FaRegBookmark } from "react-icons/fa";
+import { BiBookmarkHeart, BiSolidBookmarkHeart } from "react-icons/bi";
 
 const LikeButton = ({ handleLikeButtonClick, isLiked }) => {
 
   // isLiked가 true이면, 채워진 북마크 / false면, 빈 북마크 활성화
   const likeBtn = isLiked ?
-    <FaBookmark 
+    <BiSolidBookmarkHeart 
       onClick={handleLikeButtonClick} 
       style={{cursor: 'pointer'}}
-      size={30}
+      size={40}
+      color="pink"
        /> 
     // <Lottie 
     //   animationData={likeLottie}
     //   onClick={handleLikeButtonClick}
     //   style={{ width: 'auto', height: '80px'}} />
        :
-    <FaRegBookmark 
+    <BiBookmarkHeart
       onClick={handleLikeButtonClick} 
       style={{cursor: 'pointer'}}
-      size={30}
+      size={40}
+      color="lightgray"
        />
     
 
