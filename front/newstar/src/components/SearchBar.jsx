85a1653a5ecd993ead8a1bb842148e782e9d02fc
박은미 <amy0231@naver.com>
2024-03-18 17:28:00 +0900

    refactor: maincard css 수정

--- front/newstar/src/components/SearchBar.jsx
+++ front/newstar/src/components/SearchBar.jsx
@@ -1,6 +1,7 @@
 // 검색바
 import React, { useState } from 'react';
 import { IoSearch } from "react-icons/io5";
+import { FaRegPaperPlane } from "react-icons/fa";
 import styled from 'styled-components';
 
 // 검색 바 컨테이너 스타일
@@ -8,16 +9,41 @@ const SearchBarContainer = styled.div`
   position: relative;
   display: flex;
   align-items: center;
+  height: 60px;
+`;
+
+// 입력 필드와 아이콘들을 감싸는 컨테이너
+const InputWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  background-color: #F5F5F5;
+  border-radius: 5px;
+  padding-left: 10px; /* 좌측 패딩 추가 */
 `;
 
 // 입력 필드 스타일
 const Input = styled.input`
   width: 100%;
-  padding-right: 30px; /* 아이콘 공간 확보 */
+  border: none; /* 테두리 제거 */
+  background-color: transparent; /* 배경색 투명 */
+  height: 40px;
+  padding: 0 10px; /* 좌우 패딩 추가 */
+  &:focus {
+    outline: none; /* 포커스시 테두리 없앰 */
+  }
+`;
+
+// 경계선 스타일
+const BoundaryLine = styled.div`
+  height: 24px;
+  width: 1px;
+  background-color: #000;
+  margin: 0 12px;
 `;
 
 // 검색 아이콘 스타일
-const SearchIcon = styled(IoSearch)`
+const SearchIcon = styled(FaRegPaperPlane)`
   position: absolute;
   right: 10px;
   cursor: pointer;
@@ -38,12 +64,16 @@ export default function SearchBar({ setKeyword, onSearch }) {
 
   return (
     <SearchBarContainer>
-      <Input
-        type="text"
-        value={input}
-        placeholder="키워드를 입력하세요."
-        onChange={handleInputChange}
-      />
+      <InputWrapper>
+        <IoSearch />
+        <BoundaryLine />
+        <Input
+          type="text"
+          value={input}
+          placeholder="키워드를 입력하세요."
+          onChange={handleInputChange}
+        />
+      </InputWrapper>
       <SearchIcon onClick={handleSearchClick} />
     </SearchBarContainer>
   );
