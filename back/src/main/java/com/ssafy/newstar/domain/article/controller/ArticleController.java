d24f46d286182d5ecec87db26389b4cc0fef8893
최준영 <wjdghkswnsdu@naver.com>
2024-03-20 13:09:04 +0900

    feat: Article 페이지네이션 추가

--- back/src/main/java/com/ssafy/newstar/domain/article/controller/ArticleController.java
+++ back/src/main/java/com/ssafy/newstar/domain/article/controller/ArticleController.java
@@ -1,11 +1,16 @@
 package com.ssafy.newstar.domain.article.controller;
 
 import static com.ssafy.newstar.domain.article.dto.ArticleResponse.createArticleResponse;
+import static com.ssafy.newstar.util.response.SuccessResponseEntity.getResponseEntity;
 
 import com.ssafy.newstar.domain.article.dto.ArticleResponse;
 import com.ssafy.newstar.domain.article.service.ArticleService;
-import java.util.List;
+import com.ssafy.newstar.util.response.SuccessCode;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +20,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class ArticleController {
   private final ArticleService articleService;
   @GetMapping("/articles/{category}")
-  public List<ArticleResponse> getArticlesByCategory(@PathVariable("category") int category) {
-    return createArticleResponse(articleService.getArticlesByCategory(category));
+  public ResponseEntity<?> getArticlesByCategory(Pageable pageable,
+      @PathVariable("category") int category) {
+    Slice<ArticleResponse> response = createArticleResponse(articleService.getArticlesByCategory(pageable, category));
+    return getResponseEntity(SuccessCode.OK, response);
   }
 }
--- back/src/main/java/com/ssafy/newstar/domain/article/service/ArticleService.java
+++ back/src/main/java/com/ssafy/newstar/domain/article/service/ArticleService.java
@@ -1,17 +1,16 @@
 package com.ssafy.newstar.domain.article.service;
-
-import com.ssafy.newstar.domain.article.dto.ArticleResponse;
 import com.ssafy.newstar.domain.article.entity.Article;
 import com.ssafy.newstar.domain.article.repository.ArticleRepository;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ArticleService {
   private final ArticleRepository articleRepository;
-  public List<Article> getArticlesByCategory(int category) {
-    return articleRepository.findByCategory(category);
+  public Slice<Article> getArticlesByCategory(Pageable pageable, int category) {
+    return articleRepository.findByScategory(category, pageable);
   }
 }
