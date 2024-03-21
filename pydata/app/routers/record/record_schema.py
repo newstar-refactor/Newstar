95650363a76c3c06a8426830d957352d85ed0bdc
한성현 <tjdgus4560@naver.com>
2024-03-21 09:15:38 +0900

    fix : recode to record

--- pydata/app/routers/record/record_schema.py
+++ pydata/app/routers/record/record_schema.py
@@ -2,9 +2,9 @@ from typing import Union
 
 from pydantic import BaseModel
 
-class Recode(BaseModel):
+class Record(BaseModel):
     # Union[int, None] : 값 없으면 None
-    recodeId : Union[int, None] = None
+    recordId : Union[int, None] = None
     memberId : Union[int, None] = None
     articleId : Union[int, None] = None
     like : Union[bool, None] = None
