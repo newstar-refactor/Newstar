f8cebc2126dd07a883639d1365fee8b7a5dc588f
최준영 <wjdghkswnsdu@naver.com>
2024-03-21 15:11:12 +0900

    feat: 카테고리 검색 오류 처리

--- back/src/main/java/com/ssafy/newstar/util/response/ErrorCode.java
+++ back/src/main/java/com/ssafy/newstar/util/response/ErrorCode.java
@@ -13,7 +13,8 @@ public enum ErrorCode {
 
   // 기사
   ARTICLE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 기사를 찾을 수 없습니다."),
-
+  CATEGORY_NOT_ONE(HttpStatus.BAD_REQUEST, "카테고리는 하나만 선택해야 합니다."),
+  CATEGORY_NOT_SELECTED(HttpStatus.BAD_REQUEST, "카테고리를 적어도 하나 선택해야 합니다."),
   // 키워드
   KEYWORD_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 키워드를 찾을 수 없습니다.");
 
