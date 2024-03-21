0c7c5ede252233345941f17412027e546ec4867a
최준영 <wjdghkswnsdu@naver.com>
2024-03-21 12:28:18 +0900

    feat: article repository 설계 및 구현

--- back/src/main/java/com/ssafy/newstar/domain/article/repository/ArticleRepository.java
+++ back/src/main/java/com/ssafy/newstar/domain/article/repository/ArticleRepository.java
@@ -1,12 +1,7 @@
 package com.ssafy.newstar.domain.article.repository;
 
 import com.ssafy.newstar.domain.article.entity.Article;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-
-  Slice<Article> findByScategory(int scategory, Pageable pageable);
-}
+public interface ArticleRepository extends JpaRepository<Article, Long> , ArticleRepositoryCustom {}
---
+++ back/src/main/java/com/ssafy/newstar/domain/article/repository/ArticleRepositoryCustom.java
@@ -0,0 +1,9 @@
+package com.ssafy.newstar.domain.article.repository;
+
+import com.ssafy.newstar.domain.article.entity.Article;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+
+public interface ArticleRepositoryCustom {
+  Slice<Article> getByCategory(Pageable pageable, String bcategory, String scategory);
+}
