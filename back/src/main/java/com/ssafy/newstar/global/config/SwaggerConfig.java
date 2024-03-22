8bf2d2e08ce1dfeee6e3dc2e081445b4cd96ea64
최준영 <wjdghkswnsdu@naver.com>
2024-03-22 10:40:01 +0900

    feat: swagger Header 전역 설정 기능

---
+++ back/src/main/java/com/ssafy/newstar/global/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.ssafy.newstar.global.config;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import java.util.Collections;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SwaggerConfig {
+  public static final String AUTHORIZATION_HEADER = "X-User-Id";
+
+  @Bean
+  public OpenAPI openAPI(){
+    SecurityScheme securityScheme = new SecurityScheme()
+        .type(SecurityScheme.Type.APIKEY)
+        .in(SecurityScheme.In.HEADER).name(AUTHORIZATION_HEADER);
+    SecurityRequirement securityRequirement = new SecurityRequirement().addList(AUTHORIZATION_HEADER, Collections.emptyList());
+
+    return new OpenAPI()
+        .components(new Components().addSecuritySchemes(AUTHORIZATION_HEADER, securityScheme))
+        .security(Collections.singletonList(securityRequirement));
+  }
+
+}
