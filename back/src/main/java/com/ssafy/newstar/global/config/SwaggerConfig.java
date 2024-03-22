ecdbacacecb4b571d3290414a0b672b58d50f8fc
최준영 <wjdghkswnsdu@naver.com>
2024-03-22 12:41:10 +0900

    feat: swagger 주소 변경

--- back/src/main/java/com/ssafy/newstar/global/config/SwaggerConfig.java
+++ back/src/main/java/com/ssafy/newstar/global/config/SwaggerConfig.java
@@ -3,10 +3,10 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import java.util.Collections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 
 @Configuration
 public class SwaggerConfig {
@@ -21,7 +21,7 @@ public class SwaggerConfig {
 
     return new OpenAPI()
         .components(new Components().addSecuritySchemes(AUTHORIZATION_HEADER, securityScheme))
-        .security(Collections.singletonList(securityRequirement));
+        .security(Collections.singletonList(securityRequirement))
+        .servers(Collections.singletonList(new Server().url("https://newstar.world/api")));
   }
-
 }
