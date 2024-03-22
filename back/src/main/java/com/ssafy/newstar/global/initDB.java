baefc1be50f69655bc0617e2185b71acd633ba37
한성현 <tjdgus4560@naver.com>
2024-03-22 16:09:41 +0900

    fix : initDB 변경

--- back/src/main/java/com/ssafy/newstar/global/initDB.java
+++ back/src/main/java/com/ssafy/newstar/global/initDB.java
@@ -36,13 +36,15 @@ public class initDB {
           LocalDateTime.now(),
           100, 264,
           "https://mimgnews.pstatic.net/image/origin/658/2024/03/12/68427.jpg?type=ofullfill220_150",
-          "한 총리 “실력 있는 전문병원, 상급병원 만큼 수가 받아야” 윤석열  대통령은 12일 의과대학 증원을 포함한 의료 개혁과 관련해 “원칙대로 신속하게 추진하라”고 지시했다.");
+          "한 총리 “실력 있는 전문병원, 상급병원 만큼 수가 받아야” 윤석열  대통령은 12일 의과대학 증원을 포함한 의료 개혁과 관련해 “원칙대로 신속하게 추진하라”고 지시했다.",
+              null);
       Article article2 = Article.createArticle("韓 대통령실, 日과 국교정상화 60년 공동문서 발표에 긍정적",
           "https://n.news.naver.com/mnews/article/421/0007406931",
           LocalDateTime.now(),
           100, 264,
           "https://mimgnews.pstatic.net/image/origin/421/2024/03/12/7406931.jpg?type=ofullfill220_150",
           "16일(현지시간) 일본 도쿄의 오므라이스 식당 '렌가테이'에서 윤석열 한국 대통령과 기시다 후미오 일본 총리가 저녁 만찬 후 사진을 찍고 있다. 한일 양국 정상은 이날 12년만에 정상회담을 가졌다."
+              ,null
           );
       em.persist(article1);
       em.persist(article2);
