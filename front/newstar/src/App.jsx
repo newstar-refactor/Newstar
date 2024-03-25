8553b624429c082bc9d0d4d0695b080e5a93d4aa
박은미 <amy0231@naver.com>
2024-03-25 17:55:27 +0900

    feat(key): 사용자 맞춤 렌더링

--- front/newstar/src/App.jsx
+++ front/newstar/src/App.jsx
@@ -1,18 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom"
 import { RecoilRoot } from "recoil"
 
 
 import StartPage from "./pages/StartPage"
 import MainPage from "./pages/MainPage"
+import { useEffect } from "react"
 
-function app() {
+function App() {
   return (
     <>
       <RecoilRoot>
         <BrowserRouter>
           <Routes>
-            <Route path="/*" element={<StartPage />} />
-            <Route path="/newstar/*" element={<MainPage />} />
+            <Route path="/*" element={<StartPageWrapper />} />
+            <Route path="/newstar/*" element={<MainPageWrapper />} />
           </Routes>
         </BrowserRouter>
       </RecoilRoot>
@@ -20,4 +21,36 @@ function app() {
   )
 }
 
-export default app
+// StartPage로의 접근을 제어하는 컴포넌트
+function StartPageWrapper() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const userId = localStorage.getItem('X-USER-ID');
+    if (userId) {
+      // 사용자가 이미 로그인했다면 MainPage(또는 다른 페이지)로 리디렉션
+      navigate('/newstar');
+    }
+  }, [navigate]);
+
+  // 사용자가 로그인하지 않았을 경우에만 StartPage를 렌더링
+  return <StartPage />;
+}
+
+// MainPage로의 접근을 제어하는 컴포넌트
+function MainPageWrapper() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const userId = localStorage.getItem('X-USER-ID');
+    if (!userId) {
+      // 사용자가 로그인하지 않았다면 StartPage로 리디렉션
+      navigate('/');
+    }
+  }, [navigate]);
+
+  // 사용자가 로그인했을 경우에만 MainPage를 렌더링
+  return <MainPage />;
+}
+
+export default App;
