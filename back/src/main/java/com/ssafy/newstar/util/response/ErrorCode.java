223faa71d8ce82225c084c8058f82a99fe2eaa7c
최준영 <wjdghkswnsdu@naver.com>
2024-03-28 15:01:50 +0900

    feat: 상태 코드 설계 및 통일

--- back/src/main/java/com/ssafy/newstar/util/response/ErrorCode.java
+++ back/src/main/java/com/ssafy/newstar/util/response/ErrorCode.java
@@ -8,13 +8,14 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
   // 회원
-  KEY_NOT_FOUND(HttpStatus.NOT_FOUND, "키가 존재하지 않습니다."),
-  MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 유저를 찾을 수 없습니다."),
+  KEY_NOT_FOUND(HttpStatus.UNAUTHORIZED, "키가 존재하지 않습니다."),
+  MEMBER_NOT_FOUND(HttpStatus.UNAUTHORIZED, "해당 유저를 찾을 수 없습니다."),
 
   // 기사
   ARTICLE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 기사를 찾을 수 없습니다."),
   CATEGORY_NOT_ONE(HttpStatus.BAD_REQUEST, "카테고리는 하나만 선택해야 합니다."),
   CATEGORY_NOT_SELECTED(HttpStatus.BAD_REQUEST, "카테고리를 적어도 하나 선택해야 합니다."),
+
   // 키워드
   KEYWORD_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 키워드를 찾을 수 없습니다."),
 
