0105f5b3572aade27cb8f175e13580bf4877c914
박은미 <amy0231@naver.com>
2024-03-29 16:26:47 +0900

    refactor: 검색페이지 멘트 변경

--- front/newstar/src/pages/Search.jsx
+++ front/newstar/src/pages/Search.jsx
@@ -11,13 +11,38 @@ const SearchContainer = styled.div`
   padding: 10px 20px 70px;
 `
 
+const EmptyContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    gap: 20px;
+    padding: 20px;
+    font-size: 16px;
+    font-weight: 600;
+    min-height: calc(100vh - 190px);
+`;
+
+
 export default function Search() {
 
-  // 키워드 상태 관리
+  // 키워드 상태관리
   const [keyword, setKeyword] = useState('');
   // 필터링 된 뉴스 목록 상태관리
   const [filteredNews, setFilteredNews] = useState([]);
+  // 검색이 실행되었는지 추적 상태관리
+  const [searchPerformed, setSearchPerformed] = useState(false);
 
+
+  const handleSetKeyword = (newKeyword) => {
+    if (newKeyword.trim() !== '') {
+      setKeyword(newKeyword);
+      setSearchPerformed(true);
+    } else {
+      // 키워드가 빈 문자열이라면 검색을 실행하지 않음을 나타내기 위해 searchPerformed를 false로 설정
+      setSearchPerformed(false);
+    }
+  };
 
   useEffect(() => {
     
@@ -44,8 +69,12 @@ export default function Search() {
   
   return (
     <SearchContainer>
-      <SearchBar setKeyword={setKeyword} />
-      <SearchNewsList NewsData={filteredNews} />
+      <SearchBar setKeyword={handleSetKeyword} />
+      {searchPerformed ? (
+        <SearchNewsList NewsData={filteredNews} />
+      ) : (
+        <EmptyContainer>찾으시는 뉴스를 검색해주세요 🔍</EmptyContainer>
+      )}
     </SearchContainer>
-  )
+  );
 }
