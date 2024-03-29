5ae66b3ce44e2e3b970618d99df1398127603bc9
최준영 <wjdghkswnsdu@naver.com>
2024-03-29 15:55:49 +0900

    refactor: 기록 페이지 네이션 추가

--- back/src/main/java/com/ssafy/newstar/domain/record/repository/RecordRepository.java
+++ back/src/main/java/com/ssafy/newstar/domain/record/repository/RecordRepository.java
@@ -4,15 +4,17 @@ import com.ssafy.newstar.domain.article.entity.Article;
 import com.ssafy.newstar.domain.member.entity.Member;
 import com.ssafy.newstar.domain.record.entity.Record;
 import java.util.Optional;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface RecordRepository extends JpaRepository<Record, Long> {
     @EntityGraph(attributePaths = {"article"})
-    List<Record> findByMemberId(Long memberId);
-    List<Record> findByMemberIdAndLikes(Long memberId, boolean likes);
+    Slice<Record> findByMemberIdOrderByIdDesc(Long memberId, Pageable pageable);
+    @EntityGraph(attributePaths = {"article"})
+    Slice<Record> findByMemberIdAndLikesOrderByIdDesc(Long memberId, boolean likes, Pageable pageable);
+
     Optional<Record> findByMemberAndArticle(Member member, Article article);
 
     @EntityGraph(attributePaths = {"article"})
